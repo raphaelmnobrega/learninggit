d44256a72c05eb0ae20f6d033984f4261110574e
Raphael Mendonça <Raphael Mendona>
2016-04-21 16:44:27 -0300

    Testing diff command (edited - amend)

--- src/main.java
+++ src/main.java
@@ -5,11 +5,11 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Testing");
+		System.out.println("Trying amend");
 		
 		JOptionPane.showConfirmDialog(null, "Git Rules!");
 		
-		
+		System.out.println("Testing");
 	}
 
 }
